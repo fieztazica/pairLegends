8c9c7f4b694b7d78247dd0732507d532a4b740d6
Fiezt <dat09t@gmail.com>
2022-09-30 21:05:13 +0700

    update setup proxy

--- pl-react/src/setupProxy.js
+++ pl-react/src/setupProxy.js
@@ -1,14 +1,16 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {createProxyMiddleware} = require("http-proxy-middleware");
 
 const context = ["/weatherforecast"];
 
 module.exports = function (app) {
-    const appProxy = createProxyMiddleware(context, {
-        target: "https://localhost:5001",
+    app.use(createProxyMiddleware(context, {
+        target: `${process.env.API_HOST}`,
         secure: false,
         changeOrigin: true,
-        logger: console,
-    });
-
-    app.use(appProxy);
+        router: {
+            // when request.headers.host == 'dev.localhost:3000',
+            // override target 'http://www.example.org' to 'http://localhost:8000'
+            'dev.localhost:3000': 'http://localhost:5001',
+        },
+    }));
 };
