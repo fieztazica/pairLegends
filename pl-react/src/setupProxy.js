dbf55db36a7b4c786412da9aefe23c866241f376
Fiezt <dat09t@gmail.com>
2022-09-30 21:09:07 +0700

    proxy wth

--- pl-react/src/setupProxy.js
+++ pl-react/src/setupProxy.js
@@ -8,9 +8,7 @@ module.exports = function (app) {
         secure: false,
         changeOrigin: true,
         router: {
-            // when request.headers.host == 'dev.localhost:3000',
-            // override target 'http://www.example.org' to 'http://localhost:8000'
-            'dev.localhost:3000': 'http://localhost:5001',
+            'localhost:3000': 'http://localhost:5001',
         },
     }));
 };
