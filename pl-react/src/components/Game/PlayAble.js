dc1dc6765538c91118fbcc6383ec9cfd68533349
Fiezt <dat09t@gmail.com>
2022-10-17 22:09:04 +0700

    Loosen screen width limit

--- pl-react/src/components/Game/PlayAble.js
+++ pl-react/src/components/Game/PlayAble.js
@@ -5,53 +5,52 @@ import HomeIcon from "@mui/icons-material/Home";
 import * as util from "../../utils";
 
 export default function PlayAble({ children }) {
-  const [width, height] = util.useWindowSize();
+    const [width, height] = util.useWindowSize();
 
-  const isMobile = navigator.userAgent.match(
-    /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
-  );
+    const isMobile = navigator.userAgent.match(
+        /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
+    );
 
-  const isDev = !(
-    process.env.REACT_APP_VERCEL_ENV === "production" ||
-    process.env.NODE_ENV === "production"
-  );
+    const isDev = !(
+        process.env.REACT_APP_VERCEL_ENV === "production" ||
+        process.env.NODE_ENV === "production"
+    );
 
-  const screen = {
-    width: 800,
-    height: 600,
-  };
+    const screen = {
+        width: 1200,
+        height: 600,
+    };
 
-  return isDev ||
-    !(isMobile || width <= screen.width || height <= screen.height) ? (
-    children
-  ) : (
-    <>
-      <Typography variant="h5" component="h5">
-        {isMobile ? (
-          "The game is designed for desktop only."
-        ) : (
-          <>
-            Please make your browser wider than {screen.width}px and higher than{" "}
-            {screen.height}px.
-            <br />
-            Current width, height: {width}px, {height}px
-          </>
-        )}
-      </Typography>
-      <Button
-        to="/"
-        variant="contained"
-        component={RouterLink}
-        startIcon={<HomeIcon fontSize="small" />}
-        sx={{
-          mt: 3,
-          "&:hover": {
-            color: "primary.contrastText",
-          },
-        }}
-      >
-        Home
-      </Button>
-    </>
-  );
+    return isDev || !(isMobile || width <= screen.width || height <= screen.height) ? (
+        children
+    ) : (
+        <>
+            <Typography variant="h5" component="h5">
+                {isMobile ? (
+                    "The game is designed for desktop only."
+                ) : (
+                    <>
+                        Please make your browser wider than {screen.width}px and higher than{" "}
+                        {screen.height}px.
+                        <br />
+                        Current width, height: {width}px, {height}px
+                    </>
+                )}
+            </Typography>
+            <Button
+                to="/"
+                variant="contained"
+                component={RouterLink}
+                startIcon={<HomeIcon fontSize="small" />}
+                sx={{
+                    mt: 3,
+                    "&:hover": {
+                        color: "primary.contrastText",
+                    },
+                }}
+            >
+                Home
+            </Button>
+        </>
+    );
 }
