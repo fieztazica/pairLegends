50277f9ce99341e15de6f6dfdf6d49936a3b8802
Fiezt <dat09t@gmail.com>
2022-10-18 12:42:02 +0700

    Delete

--- Data/Repositories/IJwtManager.cs
+++ Data/Repositories/IJwtManager.cs
@@ -6,5 +6,4 @@ namespace Data.Repositories;
 public interface IJwtManager
 {
     public string Authenticate(AppUser user, IList<string> roles, bool rememberMe);
-    public DateTime GetExpireDate(string token);
 }
